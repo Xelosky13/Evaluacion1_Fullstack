120474ce9c39df67985158b34cb86015a9058a5f
IgnaciaColinir <ig.colinir@duocuc.cl>
2026-09-02 21:29:38 -0400

    Clase 0209

---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+function inyectarHeader(){
+    document.getElementById("header").innerHTML = '<section><h1>pierdelo todo</h1></section><section><a href="#inicio">inicio</a><a href="#informacion">información</a><a href="#contacto">contacto</a></section><section><p>pone tu sueldo, lo vas duplicar</p></section>';
+}
+inyectarHeader();
